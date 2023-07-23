63966fb6b430146887cb2f9b64fb0fbd13b7aab1
pythonstrup <pythonstrup@gmail.com>
2023-07-23 23:09:03 +0900

    init: spring init

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.1.2"
+    id("io.spring.dependency-management") version "1.1.2"
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
+}
+
+group = "com.onebyte"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["snippetsDir"] = file("build/generated-snippets")
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+//    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+//    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("com.mysql:mysql-connector-j")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+//    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+//    testImplementation("org.springframework.security:spring-security-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+//tasks.test {
+//    outputs.dir(snippetsDir)
+//}
+
+//tasks.asciidoctor {
+//    inputs.dir(snippetsDir)
+//    dependsOn(test)
+//}
---
+++ src/test/java/com/onebyte/life4cut/Life4cutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onebyte.life4cut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Life4cutApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
